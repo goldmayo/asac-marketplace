c80bd53da87a23312ee9a701d84dc94ebeef443f
yunji <dbswl330@gmail.com>
2023-11-27 20:47:30 +0900

    feat: shadcn 기본 button 구현

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -5,26 +5,26 @@ import { cva, type VariantProps } from 'class-variance-authority'
 import { cn } from '@/lib/utils'
 
 const buttonVariants = cva(
-  'inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-gray-950 disabled:pointer-events-none disabled:opacity-50 dark:focus-visible:ring-gray-300',
+  'inline-flex items-center justify-center whitespace-nowrap rounded-md transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-gray-950 disabled:pointer-events-none disabled:opacity-50 dark:focus-visible:ring-gray-300',
   {
     variants: {
       variant: {
-        default:
-          'bg-gray-900 text-gray-50 shadow hover:bg-gray-900/90 dark:bg-gray-50 dark:text-gray-900 dark:hover:bg-gray-50/90',
-        destructive:
-          'bg-red-500 text-gray-50 shadow-sm hover:bg-red-500/90 dark:bg-red-900 dark:text-gray-50 dark:hover:bg-red-900/90',
+        primary: 'bg-brand-primary-500 hover:bg-brand-primary-400 text-center text-white',
         outline:
-          'border border-gray-200 bg-transparent shadow-sm hover:bg-gray-100 hover:text-gray-900 dark:border-gray-800 dark:hover:bg-gray-800 dark:hover:text-gray-50',
+          'border border-brand-primary-500 bg-white hover:bg-brand-primary-100 text-center text-brand-primary-500 ',
         secondary:
-          'bg-gray-100 text-gray-900 shadow-sm hover:bg-gray-100/80 dark:bg-gray-800 dark:text-gray-50 dark:hover:bg-gray-800/80',
-        ghost: 'hover:bg-gray-100 hover:text-gray-900 dark:hover:bg-gray-800 dark:hover:text-gray-50',
-        link: 'text-gray-900 underline-offset-4 hover:underline dark:text-gray-50',
+          'border border-grayscale-100 bg-white hover:bg-gray-50',
+        disable: 'bg-grayscale-200 text-center text-white',
+        ghost: 'border border-grayscale-100 bg-white text-center text-grayscale-200',
+        
+        default:
+          'bg-grayscale-black text-gray-50 shadow hover:bg-gray-900/90 dark:bg-gray-50 dark:text-gray-900 dark:hover:bg-gray-50/90',
       },
       size: {
-        default: 'h-9 px-4 py-2',
-        sm: 'h-8 rounded-md px-3 text-xs',
-        lg: 'h-10 rounded-md px-8',
-        icon: 'h-9 w-9',
+        lg:'w-80 h-[46px] px-[26px] py-2.5 rounded-[5px] justify-center items-center gap-2.5 inline-flex text-base font-medium',
+        sm:'w-[155px] h-[47px] px-[26px] py-2.5 rounded-[5px] justify-center items-center gap-2.5 inline-flex text-sm font-semibold',
+        default: 'h-9 px-4 py-2 text-sm font-semibold',
+        icon: 'h-9 w-9 text-sm font-medium',
       },
     },
     defaultVariants: {
