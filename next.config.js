fe98e24feb100dd7b48304a260a553483947461c
yunji <dbswl330@gmail.com>
2023-11-29 14:29:41 +0900

    feat: 홈 화면의 추천탭을 디폴트 화면으로 변경

--- next.config.js
+++ next.config.js
@@ -7,6 +7,15 @@ const nextConfig = {
     })
     return config
   },
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/recommendations",
+        permanent: true,
+      },
+    ];
+  },
 }
 
 module.exports = nextConfig
