94a18c17ff79fb8a2382f2663dadc4293fc34398
yunji <dbswl330@gmail.com>
2023-11-29 11:25:13 +0900

    feat: 홈 화면 상단탭 구현

--- src/app/(main)/(home)/recommendations/page.tsx
+++ src/app/(main)/(home)/recommendations/page.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-export default function page() {
+export default function RecommendationsPage() {
   return (
     <>
         <div className='flex min-h-screen flex-col items-center justify-between p-24'>home</div>
--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -9,20 +9,22 @@ export default function MainLayout({
     children: React.ReactNode
   }) {
     const segment = useSelectedLayoutSegment()
+    console.log(segment)
     return (
 <section className="relative h-screen">
   {children}
-  <nav className="sticky bottom-0 h-14 bg-white border border-t-gray-300 flex items-center justify-around">
-    <Link className={`text-2xl ${segment === 'home' ? 'text-brand-primary-500' : 'text-black'}`} href={"/home"}>
+  <nav className="sticky bottom-0 h-14 bg-white border border-t-gray-300 text-2xl flex items-center justify-around">
+    <Link className={`${segment === '(home)' ? 'text-brand-primary-500' : 'text-black'}`} href={"/recommendations"}>
         <CiHome></CiHome>
     </Link>
-    <Link className={`text-2xl ${segment === 'category' ? 'text-brand-primary-500' : 'text-black'}`} href={"/home"}>
+    {/* 아래는 아직 페이지 구현 x (만든 후 href 수정 예정) */}
+    <Link className={`${segment === '(category)' ? 'text-brand-primary-500' : 'text-black'}`} href={"/recommendations"}>
         <CiMenuBurger></CiMenuBurger>
     </Link>
-    <Link className={`text-2xl ${segment === 'search' ? 'text-brand-primary-500' : 'text-black'}`} href={"/home"}>
+    <Link className={`${segment === '(search)' ? 'text-brand-primary-500' : 'text-black'}`} href={"/recommendations"}>
         <CiSearch></CiSearch>
     </Link>
-    <Link className={`text-2xl ${segment === 'myPage' ? 'text-brand-primary-500' : 'text-black'}`} href={"/home"}>
+    <Link className={`${segment === '(myPage)' ? 'text-brand-primary-500' : 'text-black'}`} href={"/recommendations"}>
         <CiUser></CiUser>
     </Link>
   </nav>
