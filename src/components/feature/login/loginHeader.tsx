9b2b7d2871a22e39398c44e9ad1c983bd9ad3777
HyunSJ <dudtod1596@gmail.com>
2023-11-29 14:22:19 +0900

    rename: components common 파일 추가

--- src/components/feature/login/loginHeader.tsx
+++ src/components/feature/login/loginHeader.tsx
@@ -2,7 +2,7 @@
 import { useRouter } from 'next/navigation'
 import React from 'react'
 
-import Header from '@/components/header'
+import Header from '@/components/common/header'
 import { Button } from '@/components/ui/button'
 import { CloseIcon } from '@/lib/icons'
 
