b1fadd9d64c3a9e1798d3200caf46254b815d7e7
yunji <dbswl330@gmail.com>
2023-11-30 15:34:53 +0900

    refactor: homeLayout 프리티어 적용

--- src/app/(main)/(home)/layout.tsx
+++ src/app/(main)/(home)/layout.tsx
@@ -1,19 +1,14 @@
-import React from 'react';
+import React from 'react'
 
-import HomeHeader from '@/components/feature/home/homeHeader';
-import HomeTabs from '@/components/feature/home/homeTabs';
+import HomeHeader from '@/components/feature/home/homeHeader'
+import HomeTabs from '@/components/feature/home/homeTabs'
 
-export default function HomeLayout({
-    children, 
-  }: {
-    children: React.ReactNode
-  }) {
-    return (
-<section className="relative min-h-screen">
-  <HomeHeader></HomeHeader>
-  <HomeTabs></HomeTabs>
-  {children}
-</section>
-
-    )
-  }
+export default function HomeLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <section className="relative min-h-screen">
+      <HomeHeader></HomeHeader>
+      <HomeTabs></HomeTabs>
+      {children}
+    </section>
+  )
+}
