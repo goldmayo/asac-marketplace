f01dbe6ca871a9161a2b05261bab820f3e11869a
yunji <dbswl330@gmail.com>
2023-12-01 17:18:32 +0900

    refactor: 상품 dummydata 제이슨 생성

--- src/components/feature/recommendations/lastSale.tsx
+++ src/components/feature/recommendations/lastSale.tsx
@@ -1,33 +1,8 @@
 import React from 'react'
 
+import productData from '@/../public/dummyData/productData.json'
 import SmallCard from '@/components/common/product/smallCard'
 
-const productData = [
-  {
-    productId: 1,
-    productName: '[누터스가든] CAT 누터스픽 짜먹는 간식 2종',
-    image: '/images/hotdog.svg',
-    discountRate: 35,
-    price: 6900,
-    reviewCount: 80,
-  },
-  {
-    productId: 2,
-    productName: '[누터스가든] CAT 누터스픽 짜먹는 간식 2종',
-    image: '/images/ricedog.svg',
-    discountRate: 40,
-    price: 6900,
-    reviewCount: 80,
-  },
-  {
-    productId: 3,
-    productName: '[누터스가든] CAT 누터스픽 짜먹는 간식 2종',
-    image: '/images/hotdog.svg',
-    discountRate: 35,
-    price: 6900,
-    reviewCount: 80,
-  },
-]
 export default function LastSale() {
   return (
     <div className="flex flex-col h-80 gap-4">
