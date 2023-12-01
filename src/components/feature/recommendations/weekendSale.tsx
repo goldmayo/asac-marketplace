3684bdc5ec441b21cd87d158792aca1621c11f15
yunji <dbswl330@gmail.com>
2023-12-01 18:02:14 +0900

    Feat: 추천탭 주말특가 부분 구현

---
+++ src/components/feature/recommendations/weekendSale.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+import productData from '@/../public/dummyData/productData.json'
+import BigCard from '@/components/common/product/bigCard'
+
+export default function WeekendSale() {
+  return (
+    <div className="flex flex-col w-full gap-4">
+      <p className="text-headline3 ">주말특가</p>
+      <div className="border-2 flex flex-col h-full items-center gap-4">
+        {productData.map((product) => (
+          <div key={product.productId} className="h-[269px] w-full flex flex-col items-center">
+            <BigCard product={product} />
+          </div>
+        ))}
+      </div>
+    </div>
+  )
+}
