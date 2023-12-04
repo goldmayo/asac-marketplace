60dd869289494a9f1812e16febfd942e766ad830
HyunSJ <dudtod1596@gmail.com>
2023-12-04 17:25:36 +0900

    Style: header 넓이 오타 수정

--- src/components/common/header.tsx
+++ src/components/common/header.tsx
@@ -8,7 +8,7 @@ interface Props {
 
 export default function Header({ left, center, right }: Props) {
   return (
-    <header className=" border-b border-b-2 border-grayscale-50 w-ful py-5">
+    <header className=" border-b border-b-2 border-grayscale-50 w-full py-5 px-5">
       <nav className="grid grid-cols-3 content-between items-center text-title-lg">
         {left}
         {center}
