7934306bb3d7261f109728b6f335fb22cee875e8
HyunSJ <dudtod1596@gmail.com>
2023-12-04 17:31:40 +0900

    rename: login-schema 파일명 수정

--- src/components/feature/login/loginForm.tsx
+++ src/components/feature/login/loginForm.tsx
@@ -9,7 +9,7 @@ import { z } from 'zod'
 import { Button } from '@/components/ui/button'
 import { Form, FormControl, FormField, FormItem, FormMessage } from '@/components/ui/form'
 import { Input } from '@/components/ui/input'
-import { LoginFormSchema } from '@/lib/zod/login-schema'
+import { LoginFormSchema } from '@/lib/schema/login'
 
 const FormSchema = LoginFormSchema
 
