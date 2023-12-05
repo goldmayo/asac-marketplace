b6bbbba044d25fa2f49168a40514e6eafc67140c
yunji <dbswl330@gmail.com>
2023-12-05 13:53:15 +0900

    Feat: 신상품 화면 이주의 신상 보러가기 버튼 구현

---
+++ src/components/feature/newArrivals/newArrivalButton.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function NewArrivalButton() {
+  return (
+    <button className="h-12 bg-brand-primary-500 flex items-center justify-center">
+      <div className="text-white text-body-md font-light">
+        <span className="font-semibold">이 주의 신상 </span>&nbsp;보러가기 &gt;
+      </div>
+    </button>
+  )
+}
