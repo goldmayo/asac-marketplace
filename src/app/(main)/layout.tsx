3a174bde467a09d496026e02027b2cd82015267e
HyunSJ <dudtod1596@gmail.com>
2023-12-08 15:38:39 +0900

    feat: svg 컴포넌트 적용

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -2,7 +2,8 @@
 import Link from 'next/link'
 import { useSelectedLayoutSegment } from 'next/navigation'
 import React from 'react'
-import { CiHome, CiMenuBurger, CiSearch, CiUser } from 'react-icons/ci'
+
+import { Home, Menu, Search, User } from '@/components/icons'
 
 export default function MainLayout({ children }: { children: React.ReactNode }) {
   const segment = useSelectedLayoutSegment()
@@ -11,29 +12,28 @@ export default function MainLayout({ children }: { children: React.ReactNode }) 
       {children}
       <nav className="sticky bottom-0 h-14 bg-white border border-t-gray-300 text-2xl flex items-center justify-around">
         <Link className={`${segment === '(home)' ? 'text-brand-primary-500' : 'text-black'}`} href={'/recommendations'}>
-          <CiHome />
+          <Home width={'1.5rem'} height={'1.5rem'} fill="transparent" />
         </Link>
         {/* 아래는 아직 페이지 구현 x (만든 후 href 수정 예정) */}
         <Link
           className={`${segment === '(category)' ? 'text-brand-primary-500' : 'text-black'}`}
           href={'/recommendations'}
         >
-          <CiMenuBurger />
+          <Menu width={'1.5rem'} height={'1.5rem'} className="" fill="transparent" />
         </Link>
         <Link
           className={`${segment === '(search)' ? 'text-brand-primary-500' : 'text-black'}`}
           href={'/recommendations'}
         >
-          <CiSearch />
+          <Search width={'1.5rem'} height={'1.5rem'} className="" fill="transparent" />
         </Link>
         <Link
           className={`${segment === '(myPage)' ? 'text-brand-primary-500' : 'text-black'}`}
           href={'/recommendations'}
         >
-          <CiUser />
+          <User width={'1.5rem'} height={'1.5rem'} fill="transparent" />
         </Link>
       </nav>
     </section>
   )
 }
-
