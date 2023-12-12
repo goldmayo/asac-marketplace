051a0f123c9fb2c8efee208632440232d1859a5f
yunji <dbswl330@gmail.com>
2023-12-12 14:01:10 +0900

    Refactor: new Arrival 컴포넌트 수정
    fetch 위치 수정

---
+++ src/app/(main)/(home)/new-arrivals/fetchNewArrivals.ts
@@ -0,0 +1,12 @@
+export async function fetchNewArrivalsData(params: any) {
+  const res = await fetch(`/dummyData/searchResult.json`)
+  // const res = await fetch(`/api/search/complexitem?${params}`)
+  if (!res.ok) {
+    // This will activate the closest `error.js` Error Boundary
+    throw new Error('Failed to fetch data')
+  }
+
+  //filter data with params~
+  const newArrivals = await res.json()
+  return newArrivals
+}
---
+++ src/components/feature/newArrivals/NewArrivalsList.tsx
@@ -0,0 +1,22 @@
+'use client'
+import { useSearchParams } from 'next/navigation'
+import React, { useEffect, useState } from 'react'
+
+import { fetchNewArrivalsData } from '@/app/(main)/(home)/new-arrivals/fetchNewArrivals'
+import CommonProductList from '@/components/common/product/CommonProductList'
+
+export default function NewArrivalsList() {
+  const [productList, setProductList] = useState([])
+  const filterParam = useSearchParams().get('filter')
+
+  useEffect(() => {
+    fetchNewArrivalsData(filterParam)
+      .then((data) => {
+        setProductList(data.items.content)
+      })
+      .catch((error) => {
+        console.error('data fetch 실패', error)
+      })
+  }, [filterParam])
+  return <CommonProductList productList={productList} />
+}
