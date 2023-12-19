e7aba5192401942d75fbe62607fd32a5d15f6e66
yunji <dbswl330@gmail.com>
2023-12-19 17:14:44 +0900

    Refactor: 불필요한 코드 주석처리 (추후 페이지 삭제 예정)
    newArrival, bestItem 페이지 논의 후 넣을지 제거할지 정해야함

--- src/app/(main)/(home)/new-arrivals/layout.tsx
+++ src/app/(main)/(home)/new-arrivals/layout.tsx
@@ -1,26 +1,27 @@
 import React from 'react'
-import { fetchFilterData } from '@/components/common/filters/fetchFilterData'
-import Filters from '@/components/common/filters/Filters'
+
+// import { fetchFilterData } from '@/api/resource/search'
+// import Filters from '@/components/common/filters/Filters'
 import EventBanner from '@/components/feature/newArrivals/EventBanner'
 import NewArrivalButton from '@/components/feature/newArrivals/NewArrivalButton'
 
 export default async function NewArrivalsLayout({ children }: { children: React.ReactNode }) {
-  const filterData = await fetchFilterData('newArivals')
-  const categoryCounts = filterData.categoryCounts
-  const brandCounts = filterData.brandCounts
-  const priceRange = filterData.priceRange
+  // const filterData = await fetchFilterData('newArivals')
+  // const categoryCounts = filterData.categoryCounts
+  // const brandCounts = filterData.brandCounts
+  // const priceRange = filterData.priceRange
   return (
     <div className="relative flex flex-col">
       {/* // filter안에서 total갯수 계산 */}
       <EventBanner />
       <NewArrivalButton />
-      <Filters
+      {/* <Filters
         totalEliments={0}
         categoryCounts={categoryCounts}
         brandCounts={brandCounts}
         priceRange={priceRange}
         stickyLocation={'top-24'}
-      />
+      /> */}
       {children}
     </div>
   )
