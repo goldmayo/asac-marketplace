d6779449485b3d0fc33e31f5b4c111862262321d
goldmayo <dudtod1596@gmail.com>
2024-01-01 05:08:01 +0000

    Merge branch 'main' of https://github.com/ASAC-marketplace/frontend into 77-미들웨어-header-cookie-설정-작업

--- src/app/api/oauth/callback/kakao/full/route.ts
+++ src/app/api/oauth/callback/kakao/full/route.ts
@@ -3,12 +3,17 @@ import { NextRequest, NextResponse } from 'next/server'
 
 export async function POST(req: NextRequest) {
   try {
-    const param = req.nextUrl.searchParams
-    console.log(param)
-    const token = param.get('authorization')
-    if (token) {
-      cookies().set('Authorization', `Bearer ${token}`)
-    }
+    console.log('AUTH_TOKEN req', req.cookies.get('AUTH_TOKEN'))
+    console.log('auth_token req', req.cookies.get('auth_token'))
+
+    console.log('AUTH_TOKEN cookies', cookies().get('AUTH_TOKEN'))
+    console.log('auth_token cookies', cookies().get('auth_token'))
+    // const param = req.nextUrl.searchParams
+    // console.log(param)
+    // const token = param.get('authorization')
+    // if (token) {
+    //   cookies().set('Authorization', `Bearer ${token}`)
+    // }
     return NextResponse.redirect('/recommendations')
   } catch (error) {}
 }
