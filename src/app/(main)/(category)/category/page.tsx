65825f9a3a1aad2061bfcb9ed4086780f8a18331
goldmayo <dudtod1596@gmail.com>
2024-01-04 04:56:22 +0000

    Fix: 베포 시 fetch failed 에러 fix

--- src/app/(main)/(category)/category/page.tsx
+++ src/app/(main)/(category)/category/page.tsx
@@ -4,6 +4,8 @@ import { fetchCategory } from '@/api/resource/category'
 import CategoryAccordion from '@/components/feature/category/CategoryAccordion'
 // import categoryData from '@/../public/category2.json'
 
+export const runtime = 'edge'
+
 export default async function page() {
   const category = await fetchCategory()
   return (
