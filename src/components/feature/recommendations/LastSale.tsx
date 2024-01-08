bcf67ff6ee43466140560b094af3efaafc08647b
goldmayo <dudtod1596@gmail.com>
2024-01-08 11:59:16 +0000

    Fix: 홈 - 마감세일에도 스크롤 제거

--- src/components/feature/recommendations/LastSale.tsx
+++ src/components/feature/recommendations/LastSale.tsx
@@ -9,7 +9,7 @@ export default async function LastSale() {
   return (
     <div className="flex flex-col h-80 gap-4">
       <p className="text-headline3 ">마감세일</p>
-      <div className="flex items-center overflow-x-scroll overflow-y-hidden h-[271px] gap-4">
+      <div className="flex items-center overflow-x-scroll overflow-y-hidden h-[271px] gap-4 no-scrollbar">
         {productList.map((product: ProductType) => (
           <div key={product.id} className="flex-none w-32 h-full">
             <SmallCard product={product} />
